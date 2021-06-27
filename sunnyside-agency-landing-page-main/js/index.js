65cba67edcb58ceb0071d2f35fd9c5e03650c70e
Mel <meljohnpualon@gmail.com>
2021-06-27 13:19:45 +0800

    added animation header

--- sunnyside-agency-landing-page-main/js/index.js
+++ sunnyside-agency-landing-page-main/js/index.js
@@ -1,9 +1,18 @@
 const navbarMenu = document.querySelector('.navbar-menu');
 const menuContainer = document.querySelector('.mobile-menu-container');
 
-
 const showMenu = () => {
     menuContainer.classList.toggle('mobile-menu-show');
     navbarMenu.classList.toggle('navbar-menu-clicked');
 }
 navbarMenu.addEventListener('click', showMenu)
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline();
+
+tl.from('.header-text', {
+    y: '-50%',
+    opacity: 0,
+    duration: 1
+})
