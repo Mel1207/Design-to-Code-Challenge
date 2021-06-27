16fa3a37214e8c15496bea66036a2f723dd5189d
Mel <meljohnpualon@gmail.com>
2021-06-27 08:50:00 +0800

    added styled mobile menu

--- sunnyside-agency-landing-page-main/js/index.js
+++ sunnyside-agency-landing-page-main/js/index.js
@@ -2,7 +2,8 @@ const navbarMenu = document.querySelector('.navbar-menu');
 const menuContainer = document.querySelector('.mobile-menu-container');
 
 
-// const showMenu = () => {
-//     menuContainer.classList.toggle('mobile-menu-show');
-// }
-// navbarMenu.addEventListener('click', showMenu)
+const showMenu = () => {
+    menuContainer.classList.toggle('mobile-menu-show');
+    navbarMenu.classList.toggle('navbar-menu-clicked');
+}
+navbarMenu.addEventListener('click', showMenu)
